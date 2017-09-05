3f71fe3c270c6cc94ece0e24889b2b57030eb3f6
VsevolodMart <vmarthv@gmail.com>
2017-09-05 18:18:52 +0300

    create loop for creating filds with images

--- app/js/main.js
+++ app/js/main.js
@@ -9,14 +9,13 @@ fetch('https://unsplash.it/list')
 		return;
 	  } else {
 		
-		// Examine the text in the response
-		
 		// start
 		
 		let api = response.json();
+		let mainField;
 		let field;
 		let img;
-		
+		let aside = document.querySelector('.aside');
 		
 		let btn = document.getElementsByTagName('P');
 		let Large;
@@ -38,54 +37,76 @@ fetch('https://unsplash.it/list')
 		  
 		}
 		
-		function clearField() {
-		  if (field.innerHTML == " ") {
-			return;
-		  } else {
-			field.innerHTML = "";
-		  }
-		}
-		
-		api.then(function (data) {
-		  
-		  function randomShow() {
+		// function clearField() {
+		//   if (field.innerHTML != null) {
+		// 	return;
+		//   } else {
+		// 	field.innerHTML = null;
+		//   }
+		// }
+		function randomShow() {
+		  api.then(function (data) {
+			
+			
 			let imgArr = [];
 			let authorArr = [];
 			let heightArr = [];
 			let widthArr = [];
 			let idArr = [];
-			for (let i in data) {
-			  if (data[i].width >= 200) {
-				widthArr.push(data[i].width);
-				let a = data[i].filename;
-				let arr = a.split('.');
-				let arr1 = arr[0].split('_');
-				imgArr.push(arr1[1]);
-				
-				
-				if (i >= 12) {
-				  return;
-				} else if (response.status != 200) {
-				  return;
-				} else {
-				  field = document.querySelector('.field');
-				  img = document.createElement('IMG');
-				  img.src = 'https://source.unsplash.com/' + data.width + '/' +imgArr[i] ;
-				  field.appendChild(img);
-				}
-			  } else {
-				return;
-			  }
+			
+			
+			
+			for(let k = 0; k < data.length; k++){
+			  widthArr.push(data[k].width);
+			  let array = data[k].filename;
+			  let start = array.split('.');
+			  let end = start[0].split('_');
+			  imgArr.push(end[1]);
 			}
 			
-		  }
-		  randomShow();
-		});
+			for(let o = 0; o < data.length; o++){
+			  console.log(data[o].filename);
+			}
+			
+			let i = 0;
+		 
+			for(i; i < imgArr.length; i++) {
+			  mainField = document.querySelector('.main');
+			  field = document.createElement('DIV');
+			  field.className = "field";
+			  console.log('i ' +i);
+			  let j = 0;
+
+			  for(j; j < imgArr.length; j++) {
+				if(j < 12) {
+				  img = document.createElement('IMG');
+				  img.src = 'https://source.unsplash.com/' + imgArr[j];
+				  mainField.insertBefore(field, aside);
+				  field = document.querySelector('.field');
+				  field.appendChild(img);
+				}
+				else if (response.status != 200) {
+				  console.log('status hui');
+				  continue;
+				}
+				else {
+				  i = i;
+				  j = j;
+				  console.log('j ' + j);
+				  break;
+				}
+			  }
+			}
+		  });
+		}
+		randomShow();
 		
-		//console.log(imgArr);
-		
-		api.then(function (data) {
-		  function createLarge() {
+		function sortLarge() {
+		  api.then(function (data) {
+			
+			
+			//clearField();
+			
 			let imgArr = [];
 			let authorArr = [];
 			let heightArr = [];
@@ -95,17 +116,14 @@ fetch('https://unsplash.it/list')
 			for (let i in data) {
 			  
 			  if (data[i].width >= 1500) {
-				widthArr.push(data.width);
-				imgArr.push(data.filename);
-			 
+				widthArr.push(data[i].width);
 				let array = data[i].filename;
-				let end = array.split('.');
-				let start = end[0].split('_');
-				imgArr.push(start[1]);
+				let start = array.split('.');
+				let end = start[0].split('_');
+				imgArr.push(end[1]);
 				let fileName = imgArr[i];
-			 
 				console.log(imgArr[i]);
-			 
+				
 				if (i >= 12) {
 				  break;
 				} else if (response.status != 200) {
@@ -113,87 +131,108 @@ fetch('https://unsplash.it/list')
 				} else {
 				  field = document.querySelector('.field');
 				  img = document.createElement('IMG');
-				  img.src = 'https://source.unsplash.com/' + fileName + '/' + widthArr[i];
+				  img.src = 'https://source.unsplash.com/' + widthArr[i] + fileName;
 				  field.appendChild(img);
 				}
 			  } else {
 				return;
 			  }
 			}
-		  }
+			
+		  });
 		  
-		  Large.addEventListener('click', createLarge);
-		});
+		}
 		
-		api.then(function (data) {
-		  function createMedium() {
-			clearField();
+		Large.addEventListener('click', sortLarge);
+		function sortMedium() {
+		  api.then(function (data) {
+			
+			//clearField();
+			console.log('func');
+			let imgArr = [];
+			let authorArr = [];
+			let heightArr = [];
+			let widthArr = [];
+			let idArr = [];
 			
 			for (let i in data) {
-			  if (data[i].width >= 800 && data[i].width <= 1499) {
-				widthArr.push(data.width);
-				imgArr.push(data.filename);
-				let end = imgArr[i].split('.');
-				imgArr.push(end);
-				let start = end[0];
-				let arr = start.split('_');
-				let fileName = arr[1];
-				imgArr.push(fileName);
+			  
+			  if (data[i].width >= 800 || data[i].width <= 1499) {
+				console.log('size');
+				widthArr.push(data[i].width);
+				let array = data[i].filename;
+				let start = array.split('.');
+				let end = start[0].split('_');
+				imgArr.push(end[1]);
+				
+				let fileName = imgArr[i];
 				
 				if (i >= 12) {
-				  return;
+				  break;
 				} else if (response.status != 200) {
 				  return;
 				} else {
 				  field = document.querySelector('.field');
 				  img = document.createElement('IMG');
-				  img.src = 'https://source.unsplash.com/' + fileName + '/' + widthArr[i];
+				  img.src = 'https://source.unsplash.com/' + widthArr[i] + fileName;
 				  field.appendChild(img);
 				}
 			  } else {
 				return;
 			  }
 			}
-		  }
+			
+		  });
 		  
-		  Medium.addEventListener('click', createMedium);
-		});
+		}
 		
-		api.then(function (data) {
-		  function createSmall() {
-			clearField();
+		
+		Medium.addEventListener('click', sortMedium);
+		
+		function sortSmall() {
+		  api.then(function (data) {
+			
+			//clearField();
+			console.log('func');
+			let imgArr = [];
+			let authorArr = [];
+			let heightArr = [];
+			let widthArr = [];
+			let idArr = [];
 			
 			for (let i in data) {
-			  if (data[i].width < 800) {
-				widthArr.push(data.width);
-				imgArr.push(data.filename);
-				let end = imgArr[i].split('.');
-				imgArr.push(end);
-				let start = end[0];
-				let arr = start.split('_');
-				let fileName = arr[1];
-				imgArr.push(fileName);
+			  console.log('loop');
+			  console.log(data[i]);
+			  if (data[i].width < 799) {
+				console.log('size');
+				widthArr.push(data[i].width);
+				let array = data[i].filename;
+				let start = array.split('.');
+				let end = start[0].split('_');
+				imgArr.push(end[1]);
+				let fileName = imgArr[i];
+				console.log(imgArr[i]);
 				
 				if (i >= 12) {
-				  return;
-				} else if (response.status != 200) {
-				  return;
+				  break;
 				} else if (response.status != 200) {
 				  return;
 				} else {
 				  field = document.querySelector('.field');
 				  img = document.createElement('IMG');
-				  img.src = 'https://source.unsplash.com/' + fileName + '/' + widthArr[i];
+				  img.src = 'https://source.unsplash.com/' + widthArr[i] + fileName;
 				  field.appendChild(img);
 				}
 			  } else {
+				
 				return;
 			  }
 			}
-		  }
-		  
-		  Small.addEventListener('click', createSmall);
-		});
+			
+		  });
+		}
+		
+		Small.addEventListener('click', sortSmall);
 	  }
 	})
   .catch(function(err) {
