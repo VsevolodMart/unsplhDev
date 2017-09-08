1620bf264f26f5f3310b57878c0848dd2772092f
VsevolodMart <vmarthv@gmail.com>
2017-09-08 15:31:19 +0300

    final

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,118 @@
 "use strict";
-function filter(size) {
-  let allBlocks = document.querySelectorAll("[data-size]");
-  // console.log(allBlocks);
-  for(let a in allBlocks){
-	
-	if(allBlocks.hasOwnProperty(a)){
-	  allBlocks[a].style.display = "none";
-	}
-	
+
+function filter(e) {
+  let el = e.target;
+  
+  let size = el.value;
+  
+  hide(document.querySelectorAll("[data-size]"));
+  if(size == "") {
+	show(document.querySelectorAll("[data-size]"));
+  } else {
+	show(document.querySelectorAll("[data-size=" + size + "]"));
   }
   
-  let blocks = document.querySelectorAll("[data-size="+size+"]");
-  for(let a in blocks){
-	if(blocks.hasOwnProperty(a)){
-	  blocks[a].style.display = "block";
+  
+  let numBlock = document.querySelector(".num");
+  numBlock.innerHTML = 1;
+  pagination();
+}
+
+function authorFilter(e) {
+  let el = e.target;
+  
+  let author = el.getAttribute('data-author-filter');
+  console.log(author);
+  
+  
+  hide(document.querySelectorAll("[data-author]"));
+  if(author == "") {
+	show(document.querySelectorAll("[data-author]"));
+	let sizeAll = document.querySelectorAll("[name='size']");
+	sizeAll.forEach(function(i){
+	  i.removeAttribute('disabled');
+	})
+  } else {
+	let sizeAll = document.querySelectorAll("[name='size']");
+	sizeAll.forEach(function(i){
+	  i.setAttribute('disabled','disabled')
+	});
+	show(document.querySelectorAll("[data-author='" + author + "']"));
+  }
+  
+  let numBlock = document.querySelector(".num");
+  numBlock.innerHTML = 1;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let sizeAll = document.querySelectorAll("[name='size']");
+  sizeAll.forEach(function(i){
+	i.addEventListener('change', filter, false)
+  })
+  var event = new Event("change");
+  if(document.querySelector("[name='size']:checked")){
+	document.querySelector("[name='size']:checked").dispatchEvent(event)
+  }
+}, false);
+
+
+function pagination(to) {
+  let numBlock = document.querySelector(".num");
+  let num = numBlock.textContent;
+  
+  switch(to){
+	case "r":
+	  num++;
+	  break;
+	case "l":
+	  num--;
+	  break;
+  }
+  
+  let end = 20 * num;
+  let start = end - 20;
+  
+  let size = document.querySelector("[name='size']:checked").value;
+  
+  let nodeList;
+  
+  if(size == "") {
+	nodeList = document.querySelectorAll("[data-size]");
+  } else {
+	nodeList = document.querySelectorAll("[data-size=" + size + "]");
+  }
+  
+  if(start > nodeList.length || start < 0) return;
+  
+  for (let a in nodeList) {
+	if (nodeList.hasOwnProperty(a)) {
+	  if(a >=start && a < end) {
+		nodeList[a].style.display = "block";
+	  } else {
+		nodeList[a].style.display = "none";
+	  }
+	}
+  }
+  
+  numBlock.innerHTML = num;
+}
+
+function show(nodeList) {
+  for (let a in nodeList) {
+	if (nodeList.hasOwnProperty(a)) {
+	  nodeList[a].style.display = "block";
 	}
   }
 }
+
+function hide(nodeList) {
+  for (let a in nodeList) {
+	if (nodeList.hasOwnProperty(a)) {
+	  nodeList[a].style.display = "none";
+	}
+  }
+}
+
 
 fetch('https://unsplash.it/list')
   .then(
@@ -30,56 +126,42 @@ fetch('https://unsplash.it/list')
 		// start
 		
 		let api = response.json();
-		let mainField;
-		let field;
 		let img;
-		let aside = document.querySelector('.aside');
-		
-		// let btn = document.getElementsByTagName('P');
-		// let Large;
-		// let Medium;
-		// let Small;
-		let leftArrow = document.querySelector('.left');
-		let rightArrow = document.querySelector('.right');
-		let num = document.querySelector('.num');
-		
-		// for (let i = 0; i < btn.length; i++) {
-		//     //console.log(btn);
-		//     if (btn[i].innerHTML === 'Large') {
-		//         Large = btn[i];
-		//     } else if (btn[i].innerHTML === 'Medium') {
-		//         Medium = btn[i];
-		//     } else if (btn[i].innerHTML === 'Small') {
-		//         Small = btn[i];
-		//     } else {
-		//         break
-		//     }
-		//
-		// }
-		
-		
-		// test start
 		
 		api.then(function (data) {
 		  let imageArr = [];
 		  
 		  function random() {
-			// console.log(data);
-			//  for (let l = 0; l < data.length; l++) {
-			//   imageArr.push(data[l]);
-			// }
-			imageArr = data.slice(0, 100);
+			//imageArr = data.slice(0, 10);
+			imageArr = data;
 			let j = 0;
 			let len = imageArr.length;
 			
 			let mainField = document.querySelector('.mainField');
 			let field = document.createElement('DIV');
+			
+			let authorsBlock = document.querySelector('.author');
+			let authorsUl = document.createElement('ul');
+			
+			let authorsLi = document.createElement('li');
+			let authorsInput = document.createElement('input');
+			authorsInput.setAttribute("name", "author");
+			authorsInput.setAttribute("type", "radio");
+			authorsInput.setAttribute("data-author-filter", "");
+			authorsInput.setAttribute("checked", "checked");
+			let authorText = document.createTextNode("all");
+			authorsLi.appendChild(authorText);
+			authorsLi.appendChild(authorsInput);
+			authorsBlock.appendChild(authorsUl);
+			authorsUl.appendChild(authorsLi);
+			authorsInput.addEventListener('change', authorFilter, false);
+			
 			field.setAttribute('class', 'field');
-			let offset = 0;
 			
 			for (let i = 0; i < len; i++) {
 			  let currentImg = imageArr[i];
 			  let width = currentImg.width;
+			  let author = currentImg.author;
 			  let cut = currentImg.filename;
 			  
 			  cut = cut.split('.');
@@ -90,184 +172,77 @@ fetch('https://unsplash.it/list')
 			  // field = document.querySelector('.field');
 			  img.setAttribute('class', 'img');
 			  let size = "medium";
-			  if(width > 1500){
+			  if (width > 1500) {
 				size = "large";
-			  } else if(width < 799){
+			  } else if (width < 799) {
 				size = "small";
 			  }
-			  
-			  
 			  img.setAttribute('data-size', size);
-			  field.appendChild(img);
-			  mainField.appendChild(field);
+			  img.setAttribute('data-author', author);
+			  img.addEventListener('click', showFull, false);
 			  
-			  
-			  function pagination() {
-				let count = 0;
-				let pages = document.querySelectorAll('.field');
-				let numPage = document.querySelector('.num');
-				if (count < pages.length && count > pages.length) {
-				  return
-				} else {
-				  count++;
-				}
-				function toRight(event) {
-				  let elem = event.target;
-				  let pages = document.querySelectorAll('.field');
-				  
-				  if (event) {
-					
-					count++;
-					numPage.innerHTML = count;
-				  }
-				}
+			  //authors
+			  if(!document.querySelector('[data-author-filter="'+author+'"]')){
+				let authorsLi = document.createElement('li');
+				let authorsInput = document.createElement('input');
+				authorsInput.setAttribute("name", "author");
+				authorsInput.setAttribute("type", "radio");
+				authorsInput.setAttribute("data-author-filter", author);
+				let authorText = document.createTextNode(author);
 				
-				rightArrow.addEventListener('click', toRight);
-				
-				function toleft(event) {
-				  let elem = event.target;
-				  let pages = document.querySelectorAll('.field');
-				  
-				  
-				  if (event) {
-					pages[count].style.visibility = 'hidden';
-					pages[count - 1].style.visibility = 'visible';
-					count--;
-					numPage.innerHTML = count;
-				  }
-				}
-				
-				leftArrow.addEventListener('click', toleft)
+				authorsLi.appendChild(authorText);
+				authorsLi.appendChild(authorsInput);
+				authorsBlock.appendChild(authorsUl);
+				authorsUl.appendChild(authorsLi);
+				authorsInput.addEventListener('change', authorFilter, false)
 			  }
 			  
-			  pagination();
+			  if ( i >= 20 ) {
+				img.style.display = "none";
+			  }
 			  
-			  // if (offset >= len) break;
-			  //
-			  // field = document.createElement('DIV');
-			  // field.className = "field";
-			  //
-			  // for (j = 0; j < 12; j++) {
-			  //     if (offset >= len) break;
-			  //     let currentImg = imageArr[offset]
-			  //
-			  //
-			  //     let width = currentImg.width;
-			  //     let cut = currentImg.filename;
-			  //
-			  //     cut = cut.split('.');
-			  //     cut = cut[0].substring(5);
-			  //
-			  //     img = document.createElement('IMG');
-			  //     img.src = 'https://source.unsplash.com/' + cut;
-			  //     // field = document.querySelector('.field');
-			  //     img.setAttribute('class', 'img');
-			  //     let size = "medium"
-			  //     if(width > 1500){
-			  //         size = "large"
-			  //     } else if(width < 799){
-			  //         size = "small"
-			  //     }
-			  //
-			  //
-			  //     img.setAttribute('data-size', size);
-			  //     field.appendChild(img);
-			  //     offset++
-			  // }
-			  // mainField.appendChild(field);
-			  
+			  field.appendChild(img);
+			  mainField.appendChild(field);
 			}
+			authorsBlock.appendChild(authorsUl);
 		  }
 		  
 		  random();
-		  
-		  function clearField() {
-			field = document.querySelector('.field');
-			field.innerHTML = ' '
-		  }
-		  
-		  
-		  
-		  // function showLarge() {
-		  //     clearField();
-		  //
-		  //     let j = 0;
-		  //     let count = 0;
-		  //
-		  //     for (let i = 0; i < imageArr.length; i++) {
-		  //         mainField = document.querySelector('.mainField');
-		  //         field = document.createElement('DIV');
-		  //         field.className = "field";
-		  //         for (j; j < imageArr.length; j++) {
-		  //             let width = imageArr[j].width;
-		  //             if (width > 1500) {
-		  //                 let cut = imageArr[j].filename;
-		  //                 cut = cut.split('.');
-		  //                 cut = cut[0].substring(5);
-		  //                 mainField.appendChild(field);
-		  //                 img = document.createElement('IMG');
-		  //                 img.src = 'https://source.unsplash.com/' + cut;
-		  //                 field = document.querySelector('.field');
-		  //                 img.setAttribute('data-width', width);
-		  //                 img.setAttribute('class', 'img');
-		  //                 field.appendChild(img);
-		  //                 if (count <= 12) {
-		  //                     count++;
-		  //                     j = j++;
-		  //                 } else {
-		  //                     console.log(count);
-		  //                     count = 0;
-		  //                     break
-		  //                 }
-		  //                 console.log(j + ' j');
-		  //             }
-		  //         }
-		  //         if (j === data.length) break;
-		  //         console.log('i ' + i);
-		  //     }
-		  // }
-		  
-		  // Large.addEventListener('click', filter);
-		  
-		  
-		  let getField = document.querySelector('.field');
 		  
 		  function showFull(event) {
 			event.stopPropagation();
 			event.preventDefault();
 			let elem = event.target;
-			if (elem !== elem.parentNode) {
-			  let getUrl = elem.getAttribute('SRC');
-			  let main = document.querySelector('body');
-			  let container = document.createElement('DIV');
-			  let img = document.createElement('IMG');
-			  container.setAttribute('class', 'container');
-			  img.src = getUrl;
-			  main.appendChild(container);
-			  container.appendChild(img);
-			  let newStyles = document.createElement('style');
-			  document.head.append(newStyles);
-			  newStyles.innerHTML = ".container {" +
-				"background-color: rgba(0, 0, 0, 1);" +
-				"}" +
-				".body {" +
-				"background-color: rgba(0, 0, 0, 1);" +
-				"}";
-			  let close = document.createElement('i');
-			  close.setAttribute('class', 'i');
-			  //close.setAttribute('onClick', 'removeFull()');
-			  close.innerHTML = '&#10006';
-			  container.insertBefore(close, img);
-			  
-			  
-			} else {
-			  return
-			}
+			let getUrl = elem.getAttribute('SRC');
+			let main = document.querySelector('body');
+			let container = document.createElement('DIV');
+			let img = document.createElement('IMG');
+			container.setAttribute('class', 'container');
+			img.src = getUrl;
+			main.appendChild(container);
+			container.appendChild(img);
+			let newStyles = document.createElement('style');
+			document.head.append(newStyles);
+			newStyles.innerHTML = ".container {" +
+			  "background-color: rgba(0, 0, 0, 1);" +
+			  "}" +
+			  ".body {" +
+			  "background-color: rgba(0, 0, 0, 1);" +
+			  "}";
+			let close = document.createElement('i');
 			
+			close.addEventListener('click', function () {
+			  let c = document.querySelector('.container');
+			  console.log(c);
+			  if(c){
+				c.parentNode.removeChild(c);
+			  }
+			}, false);
+			close.setAttribute('class', 'i');
+			//close.setAttribute('onClick', 'removeFull()');
+			close.innerHTML = '&#10006';
+			container.insertBefore(close, img);
 		  }
-		  
-		  //getField.addEventListener('click', showFull, false);
-		  //getField.removeEventListener('dbclick', showFull, false);
 		  
 		  let body = document.querySelector('body');
 		  let close = document.querySelector('.container');
@@ -279,234 +254,10 @@ fetch('https://unsplash.it/list')
 			close.removeChild(close);
 		  }
 		  
-		  
-		  // console.log(close);
-		  
 		  document.addEventListener('DOMContentLoaded', function () {
 			close.addEventListener('click', removeFull, false);
 		  });
-		  
-		  
 		});
-		/// test end
-		
-		
-		// noinspection JSAnnotator
-		
-		
-		
-		// function clearField() {
-		//   if (field.innerHTML != null) {
-		//     return;
-		//   } else {
-		//     field.innerHTML = null;
-		//   }
-		// }
-		
-		
-		// function randomShow() {
-		//   api.then(function (data) {
-		//     let array = [];
-		//     let imgArr = [];
-		//     let authorArr = [];
-		//     let heightArr = [];
-		//     let widthArr = [];
-		//     let idArr = [];
-		//
-		//     for(let i = 0; i < data.length; i++){
-		//       widthArr.push(data[i].width);
-		//       let array = data[i].filename;
-		//       let end = array.split('.');
-		//       let start = end[0].substring(5);
-		//       imgArr.push(start);
-		//       console.log(imgArr[i] + '  - ' + i);
-		//     }
-		//
-		//
-		//     //loop start
-		//     let j = 0;
-		//     let count = 0;
-		//     for(let i = 0; i < imgArr.length; i++) {
-		//       console.log( 'count ' + count );
-		//
-		//       if(count < 12){
-		//        mainField = document.querySelector('.mainField');
-		//        field = document.createElement('DIV');
-		//        field.className = "field";
-		//       } else {
-		//        count = 0;
-		//        continue;
-		//       }
-		//
-		//       for(j ; j < imgArr.length; j++) {
-		//        if(count >= 12){
-		//          break;
-		//        } else {
-		//          img = document.createElement('IMG');
-		//          img.src = 'https://source.unsplash.com/' + imgArr[j];
-		//          mainField.appendChild(field);
-		//          field = document.querySelector('.field');
-		//          field.appendChild(img);
-		//          count++;
-		//          j = j++;
-		//        }
-		//        console.log('j ' + j);
-		//       }
-		//
-		//       if(j === imgArr.length) break;
-		//       console.log(i);
-		//     }
-		//     //loo end
-		//   });
-		// }
-		// randomShow();
-		
-		// function pagination() {
-		//   function toRight() {
-		//     let newStyles = document.createElement('style');
-		//     document.head.append(newStyles);
-		//     newStyles.innerHTML = ".field {" +
-		//       "margin-left: 100px;" +
-		//       "}"
-		//   }
-		//   rightArrow.addEventListener('click', toRight)
-		// }
-		// pagination();
-		
-		//
-		//
-		//     function sortLarge() {
-		//       api.then(function (data) {
-		//
-		//
-		//        //clearField();
-		//
-		//        let imgArr = [];
-		//        let authorArr = [];
-		//        let heightArr = [];
-		//        let widthArr = [];
-		//        let idArr = [];
-		//
-		//        for (let i in data) {
-		//
-		//          if (data[i].width >= 1500) {
-		//           widthArr.push(data[i].width);
-		//           let array = data[i].filename;
-		//           let start = array.split('.');
-		//           let end = start[0].split('_');
-		//           imgArr.push(end[1]);
-		//           let fileName = imgArr[i];
-		//           console.log(imgArr[i]);
-		//
-		//           if (i >= 12) {
-		//             break;
-		//           } else if (response.status != 200) {
-		//             return;
-		//           } else {
-		//             field = document.querySelector('.field');
-		//             img = document.createElement('IMG');
-		//             img.src = 'https://source.unsplash.com/' + widthArr[i] + fileName;
-		//             field.appendChild(img);
-		//           }
-		//          } else {
-		//           return;
-		//          }
-		//        }
-		//
-		//       });
-		//
-		//     }
-		//
-		//     Large.addEventListener('click', sortLarge);
-		//     function sortMedium() {
-		//       api.then(function (data) {
-		//
-		//        //clearField();
-		//        console.log('func');
-		//        let imgArr = [];
-		//        let authorArr = [];
-		//        let heightArr = [];
-		//        let widthArr = [];
-		//        let idArr = [];
-		//
-		//        for (let i in data) {
-		//
-		//          if (data[i].width >= 800 || data[i].width <= 1499) {
-		//           console.log('size');
-		//           widthArr.push(data[i].width);
-		//           let array = data[i].filename;
-		//           let start = array.split('.');
-		//           let end = start[0].split('_');
-		//           imgArr.push(end[1]);
-		//
-		//           let fileName = imgArr[i];
-		//
-		//           if (i >= 12) {
-		//             break;
-		//           } else if (response.status != 200) {
-		//             return;
-		//           } else {
-		//             field = document.querySelector('.field');
-		//             img = document.createElement('IMG');
-		//             img.src = 'https://source.unsplash.com/' + widthArr[i] + fileName;
-		//             field.appendChild(img);
-		//           }
-		//          } else {
-		//           return;
-		//          }
-		//        }
-		//
-		//       });
-		//
-		//     }
-		//
-		//
-		//     Medium.addEventListener('click', sortMedium);
-		//
-		//     function sortSmall() {
-		//       api.then(function (data) {
-		//
-		//        //clearField();
-		//        console.log('func');
-		//        let imgArr = [];
-		//        let authorArr = [];
-		//        let heightArr = [];
-		//        let widthArr = [];
-		//        let idArr = [];
-		//
-		//        for (let i in data) {
-		//          console.log('loop');
-		//          console.log(data[i]);
-		//          if (data[i].width < 799) {
-		//           console.log('size');
-		//           widthArr.push(data[i].width);
-		//           let array = data[i].filename;
-		//           let start = array.split('.');
-		//           let end = start[0].split('_');
-		//           imgArr.push(end[1]);
-		//           let fileName = imgArr[i];
-		//           console.log(imgArr[i]);
-		//
-		//           if (i >= 12) {
-		//             break;
-		//           } else if (response.status != 200) {
-		//             return;
-		//           } else {
-		//             field = document.querySelector('.field');
-		//             img = document.createElement('IMG');
-		//             img.src = 'https://source.unsplash.com/' + widthArr[i] + fileName;
-		//             field.appendChild(img);
-		//           }
-		//          } else {
-		//
-		//           return;
-		//          }
-		//        }
-		//
-		//       });
-		//     }
-		//
-		//     Small.addEventListener('click', sortSmall);
 	  }
 	})
   .catch(function (err) {
